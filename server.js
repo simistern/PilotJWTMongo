0b30b3e79139af41ae683155f17d63794a9f6701
Simon <simon160@gmail.com>
2016-02-10 16:01:24 -0500

    only hashed passwords stored, only hashed passwords checked

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // =======================
-// get the packages we need ============
+// Get All Packages ======
 // =======================
 var express     = require('express');
 var app         = express();
@@ -7,38 +7,33 @@ var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
 var bcrypt = require("bcrypt");
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-var User   = require('./app/models/user'); // get our mongoose model
+var jwt    = require('jsonwebtoken');
+var config = require('./config');
+var User   = require('./app/models/user');
 
 // =======================
 // configuration =========
 // =======================
-var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
-mongoose.connect(config.database); // connect to database
-app.set('superSecret', config.secret); // secret variable
+var port = process.env.PORT || 3000;
+mongoose.connect(config.database);
+app.set('superSecret', config.secret);
 
-// use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// use morgan to log requests to the console
 app.use(morgan('dev'));
 
 // =======================
 // routes ================
 // =======================
-// basic route
 app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
-// API ROUTES -------------------
-// get an instance of the router for api routes
 var apiRoutes = express.Router();
 
+//Checks if username/password correct (attach to login view in front end)
 apiRoutes.post('/authenticate', function(req, res) {
-  console.log("Testing request body " + JSON.stringify(req.body));
   User.findOne({
     name: req.body.name
   }, function(err, user) {
@@ -49,21 +44,14 @@ apiRoutes.post('/authenticate', function(req, res) {
       res.json({ success: false, message: 'Authentication failed. User not found.' });
     } else if (user) {
 
-      var salt = bcrypt.genSaltSync(10);
-      var hash = bcrypt.hashSync("cruz", salt);
-
-      // check if password matches
-      if (user.password != req.body.password) {
+      // check if hashed password matches
+      if (!(bcrypt.compareSync(req.body.password, user.password))) {
         res.json({ success: false, message: 'Authentication failed. Wrong password.' });
       } else {
-
-        // if user is found and password is right
         // create a token
         var token = jwt.sign(user, app.get('superSecret'), {
-          expiresInMinutes: 1440 // expires in 24 hours
+          expiresInMinutes: 1440
         });
-
-        // return the information including token as JSON
         res.json({
           success: true,
           message: 'Enjoy your token!',
@@ -75,9 +63,7 @@ apiRoutes.post('/authenticate', function(req, res) {
 });
 
 apiRoutes.use(function(req, res, next) {
-  // check header or url parameters or post parameters for token
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  // decode token
   if (token) {
     // verifies secret and checks exp
     jwt.verify(token, app.get('superSecret'), function(err, decoded) {
@@ -91,7 +77,6 @@ apiRoutes.use(function(req, res, next) {
     });
 
   } else {
-
     // if there is no token
     // return an error
     return res.status(403).send({
@@ -117,12 +102,10 @@ apiRoutes.get('/superadmin', function(req,res, next){
   }
 })
 
-// route to show a random message (GET http://localhost:8080/api/)
 apiRoutes.get('/', function(req, res) {
   res.json({ message: 'Welcome to the coolest API on earth!' });
 });
 
-// route to return all users (GET http://localhost:8080/api/users)
 apiRoutes.get('/users', function(req, res) {
   User.find({}, function(err, users) {
     res.json(users);
